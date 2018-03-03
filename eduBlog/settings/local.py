72d9d4af0664bba3438589bd9e149c2d1fbe99bf
Gurjeet Singh <developer.gurjeet@gmail.com>
2018-03-03 12:32:13 +0530

    Intial setup

---
+++ eduBlog/settings/local.py
@@ -0,0 +1,14 @@
+from eduBlog.settings.base import *
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'eduBlog',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+    }
+}
